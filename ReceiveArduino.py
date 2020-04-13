2e644418e9e2d9cf32a4091630a39ea6b2b75911
Konsta Toivonen <konsta.toivonen@protonmail.com>
2020-04-13 20:25:03 +0300

    Set radio PALevel to MAX

--- ReceiveArduino.py
+++ ReceiveArduino.py
@@ -13,7 +13,7 @@ radio.begin(0, 17)
 radio.setPayloadSize(32)
 radio.setChannel(0x76)
 radio.setDataRate(NRF24.BR_1MBPS)
-radio.setPALevel(NRF24.PA_MIN)
+radio.setPALevel(NRF24.PA_MAX)
 
 radio.setAutoAck(True)
 radio.enableDynamicPayloads()

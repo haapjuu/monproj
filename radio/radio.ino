263b239ba7114452cbf609181478cbeda6441eb3
Konsta Toivonen <konsta.toivonen@protonmail.com>
2020-04-13 20:11:59 +0300

    testing receiving without sleep

--- radio/radio.ino
+++ radio/radio.ino
@@ -4,24 +4,25 @@
 
 // ce, csn pins
 RF24 radio(9, 10);
- 
+const byte address[6] = "00001";
+
 void setup(void){
   Serial.begin(9600);
   radio.begin();
   radio.setPALevel(RF24_PA_MAX);
   radio.setChannel(0x76);
-  radio.openWritingPipe(0xF0F0F0F0E1LL);
+  radio.openWritingPipe(address);
   radio.enableDynamicPayloads();
   radio.powerUp();
   radio.stopListening();
- 
+
 }
- 
+
 void loop(void){
   Serial.println("Attempting to send data");
   const char text[] = "Thist is a test, please ignore";
   radio.write(&text, sizeof(text));
   Serial.println("Sending data");
   delay(1000);
- 
+
 }

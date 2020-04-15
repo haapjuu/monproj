0e484535349484e2ba56e646883cf1b73ae29e39
Konsta Toivonen <konsta.toivonen@protonmail.com>
2020-04-15 16:53:57 +0300

    set PA level to MIN

--- ReceiveArduino.py
+++ ReceiveArduino.py
@@ -13,7 +13,7 @@ radio.begin(0, 17)
 radio.setPayloadSize(32)
 radio.setChannel(0x76)
 radio.setDataRate(NRF24.BR_1MBPS)
-radio.setPALevel(NRF24.PA_HIGH)
+radio.setPALevel(NRF24.PA_MIN)
 
 radio.setAutoAck(True)
 radio.enableDynamicPayloads()
@@ -23,10 +23,10 @@ radio.openReadingPipe(1, pipes[1])
 radio.printDetails()
 radio.startListening()
 
-while(1):
-    # ackPL = [1]
+while True:
     while not radio.available(0):
         time.sleep(1 / 100)
+
     receivedMessage = []
     radio.read(receivedMessage, radio.getDynamicPayloadSize())
     print("Received: {}".format(receivedMessage))
@@ -38,4 +38,3 @@ while(1):
         if (n >= 32 and n <= 126):
             string += chr(n)
     print("Out received message decodes to: {}".format(string))
-    #time.sleep(1)

b94ce7226e2bce70149d2c5b3ba5d2803d97edf4
Konsta Toivonen <konsta.toivonen@protonmail.com>
2020-04-15 15:48:53 +0300

    Trying repair the mess I made with git

---
+++ ReceiveArduino.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+from lib_nrf24 import NRF24
+import time
+import spidev
+
+GPIO.setmode(GPIO.BCM)
+
+pipes = [[0xE0, 0xE0, 0xF1, 0xF1, 0xE0], [0xF1, 0xF1, 0xF0, 0xF0, 0xE0]]
+
+radio = NRF24(GPIO, spidev.SpiDev())
+radio.begin(0, 17)
+
+radio.setPayloadSize(32)
+radio.setChannel(0x76)
+radio.setDataRate(NRF24.BR_1MBPS)
+radio.setPALevel(NRF24.PA_HIGH)
+
+radio.setAutoAck(True)
+radio.enableDynamicPayloads()
+radio.enableAckPayload()
+
+radio.openReadingPipe(1, pipes[1])
+radio.printDetails()
+radio.startListening()
+
+while(1):
+    # ackPL = [1]
+    while not radio.available(0):
+        time.sleep(1 / 100)
+    receivedMessage = []
+    radio.read(receivedMessage, radio.getDynamicPayloadSize())
+    print("Received: {}".format(receivedMessage))
+
+    print("Translating the receivedMessage into unicode characters")
+    string = ""
+    for n in receivedMessage:
+        # Decode into standard unicode set
+        if (n >= 32 and n <= 126):
+            string += chr(n)
+    print("Out received message decodes to: {}".format(string))
+    #time.sleep(1)

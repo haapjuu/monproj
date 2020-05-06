a5562d0ec5ebdab64645d25dab5a30c953a3ee33
konstatoivonen <konsta.toivonen@protonmail.com>
2020-05-06 20:19:19 +0300

    Added logging to ReceiveArduino.py. Installed Apache2 server and created index page displaying the logs.

--- ReceiveArduino.py
+++ ReceiveArduino.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 from lib_nrf24 import NRF24
 import time
 import spidev
+import datetime
 
 GPIO.setmode(GPIO.BCM)
 
@@ -23,6 +24,9 @@ radio.openReadingPipe(1, pipes[1])
 radio.printDetails()
 radio.startListening()
 
+f= open("logs.txt", "a+")
+now = datetime.datetime.now()
+
 while True:
     while not radio.available(0):
         time.sleep(0.01)
@@ -36,4 +40,7 @@ while True:
     for n in receivedMessage:
         if (n >= 32 and n <= 126):
             string += chr(n)
-    print("Out received message decodes to: {}".format(string))
+    print("Our received message decodes to: {}".format(string))
+    f.write(now.strftime("%Y-%b-%d %H:%M")+" "+"{}".format(string)+"\n")
+    
+

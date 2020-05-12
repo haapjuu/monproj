671905295653588a5a3b580449e91bce36877030
Konsta Toivonen <konsta.toivonen@protonmail.com>
2020-05-12 19:12:56 +0300

    Updated keyboard

--- accesscontrol/accesscontrol.ino
+++ accesscontrol/accesscontrol.ino
@@ -1,30 +1,32 @@
-#include <Servo.h>
 #include <Keypad.h>
 #include<SPI.h>
 #include<RF24.h>
 
-Servo servo_Motor;
+const byte ROWS = 3;
+const byte COLS = 3;
+
+int RelayControlPin = 2;
+
 const char* password = "2222";
 int position = 0;
-const byte ROWS = 4;
-const byte COLS = 3;
+
 RF24 radio(9, 10);
+
 
 char keys[ROWS][COLS] = {
   {'1', '2', '3'},
   {'4', '5', '6'},
   {'7', '8', '9'},
-  {'*', '0', '#'}
 };
 
-byte rowPins[ROWS] = {8, 7, 6, 5};
-byte colPins[COLS] = {4, 3, 2};
+byte rowPins[ROWS] = {8, 7, 6};
+byte colPins[COLS] = {5, 4, 3};
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 
 void setup() {
-  servo_Motor.attach(8);
   Serial.begin(9600);
+  pinMode(RelayControlPin, OUTPUT);
   setLocked(true);
 
   radio.begin();
@@ -33,44 +35,42 @@ void setup() {
   radio.openWritingPipe(0xF0f0f0f0E1LL);
   radio.enableDynamicPayloads();
   radio.powerUp();
-
-
-
 }
 
 void loop() {
   char key = keypad.getKey();
 
-  if (key != NO_KEY){
+  if (key != NO_KEY) {
     Serial.println(key);
-  }
-
-  if (key == '*') {
-      position = 0;
-      setLocked(true);
-      const char lockedmessage[] = "Locked";
-      radio.write(&lockedmessage, sizeof(lockedmessage));
   }
 
   if (key == password[position]) {
     position ++;
+
   }
+
+
 
   if (position == 4) {
     setLocked(false);
-    const char unlockedmessage[] = "Unlocked";
-    radio.write(&unlockedmessage, sizeof(unlockedmessage));
+    delay(5000);
+    setLocked(true);
+    position = 0;
   }
+
+
   delay(100);
 }
 
 void setLocked(int locked) {
   if (locked) {
-    servo_Motor.write(10);
+    digitalWrite(RelayControlPin, LOW);
     Serial.println("Locking");
   }
   else {
-    servo_Motor.write(170);
+    digitalWrite(RelayControlPin, HIGH);
     Serial.println("Unlocking");
+    const char unlockedmessage[] = "Unlocked";
+    radio.write(&unlockedmessage, sizeof(unlockedmessage));
   }
 }
--- tests/keyboard_lock_test/keyboard_lock_test.ino
+++ tests/keyboard_lock_test/keyboard_lock_test.ino
@@ -1,11 +1,16 @@
 #include <Keypad.h>
+#include<SPI.h>
+#include<RF24.h>
 
 const byte ROWS = 3;
 const byte COLS = 3;
-const int lockPin = 2;
+
+int RelayControlPin = 2;
+
 const char* password = "2222";
 int position = 0;
 
+RF24 radio(9, 10);
 
 
 char keys[ROWS][COLS] = {
@@ -21,8 +26,15 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup() {
   Serial.begin(9600);
-  pinMode(lockPin, OUTPUT);
+  pinMode(RelayControlPin, OUTPUT);
   setLocked(true);
+
+  radio.begin();
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setChannel(0x74);
+  radio.openWritingPipe(0xF0f0f0f0E1LL);
+  radio.enableDynamicPayloads();
+  radio.powerUp();
 }
 
 void loop() {
@@ -34,7 +46,10 @@ void loop() {
 
   if (key == password[position]) {
     position ++;
+
   }
+
+  
 
   if (position == 4) {
     setLocked(false);
@@ -49,11 +64,13 @@ void loop() {
 
 void setLocked(int locked) {
   if (locked) {
-    digitalWrite(lockPin, LOW);
+    digitalWrite(RelayControlPin, LOW);
     Serial.println("Locking");
   }
   else {
-    digitalWrite(lockPin, HIGH);
+    digitalWrite(RelayControlPin, HIGH);
     Serial.println("Unlocking");
+    const char unlockedmessage[] = "Unlocked";
+    radio.write(&unlockedmessage, sizeof(unlockedmessage));
   }
 }
